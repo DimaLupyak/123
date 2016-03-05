0b2501729cb219d7b1440788dfb850491a855f21
Dima Lupyak <dima.lupyak@gmail.com>
2016-03-05 03:23:36 +0200

    Fixed saving enabling and filename

--- Graph Based Image Segmentation/View/MainWindow.xaml.cs
+++ Graph Based Image Segmentation/View/MainWindow.xaml.cs
@@ -8,15 +8,30 @@ namespace ImageSegmentation.View
     public enum ExampleEnum { FooBar, BarFoo }
     public partial class MainWindow : ThemedWindow
     {
+        MainViewModel viewModel;
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new MainViewModel();
+            viewModel = new MainViewModel();
+            this.DataContext = viewModel;
             dataStructure.ItemsSource = Enum.GetValues(typeof(DataStructure));
             sortModification.ItemsSource = Enum.GetValues(typeof(SortModification));
             margeHeuristic.ItemsSource = Enum.GetValues(typeof(MargeHeuristic));
             connectingMethods.ItemsSource = Enum.GetValues(typeof(ConnectingMethod));
             difType.ItemsSource = Enum.GetValues(typeof(ColorDifference));
+        }
+
+        private void Image_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // Assuming you have one file that you care about, pass it off to whatever
+                // handling code you have defined.
+                viewModel.OpenImage(files[0]);
+            }
         }
     }
 }
--- ImageSegmentationViewModel/ViewModels/MainViewModel.cs
+++ ImageSegmentationViewModel/ViewModels/MainViewModel.cs
@@ -7,7 +7,9 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace ImageSegmentation.ViewModel
 {
@@ -22,21 +24,21 @@ namespace ImageSegmentation.ViewModel
             }
             set
             {
-                perfomanceInfo  = value;
+                perfomanceInfo = value;
                 RaisePropertyChanged("PerfomanceInfo");
             }
         }
 
-        private int _k = 100;
+        private int k = 100;
         public int K
         {
             get
             {
-                return _k;
+                return k;
             }
             set
             {
-                _k = value;
+                k = value;
                 RaisePropertyChanged("K");
             }
         }
@@ -146,19 +148,25 @@ namespace ImageSegmentation.ViewModel
             get { return _openImageCommand ?? (_openImageCommand = new RelayCommand(OpenImage)); }
         }
 
-        private RelayCommand _gaussianImageCommand;
+        private RelayCommand _saveImageCommand;
+        public RelayCommand SaveImageCommand
+        {
+            get { return _saveImageCommand ?? (_saveImageCommand = new RelayCommand(SaveImage)); }
+        }
+
+        private RelayCommand gaussianImageCommand;
         public RelayCommand GaussianImageCommand
         {
-            get { return _gaussianImageCommand ?? (_gaussianImageCommand = new RelayCommand(GaussianImage)); }
+            get { return gaussianImageCommand ?? (gaussianImageCommand = new RelayCommand(GaussianImage)); }
         }
-        
 
-        private RelayCommand _segmentImageCommand;
+
+        private RelayCommand segmentImageCommand;
         public RelayCommand SegmentImageCommand
         {
-            get { return _segmentImageCommand ?? (_segmentImageCommand = new RelayCommand(SegmentImage)); }
+            get { return segmentImageCommand ?? (segmentImageCommand = new RelayCommand(SegmentImage)); }
         }
-        
+
         private void OpenImage()
         {
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
@@ -168,15 +176,40 @@ namespace ImageSegmentation.ViewModel
 
             if (result == true)
             {
+                OpenImage(dlg.FileName);
+            }
+
+        }
+        
+        public void OpenImage(string fileName)
+        {
+            try
+            {
                 SegmentedImage = null;
-                OriginImage = new ImageViewModel(new Bitmap(dlg.FileName));
+                OriginImage = new ImageViewModel(new Bitmap(fileName));
 
                 ClearPerfomanceInfo();
                 RaisePropertyChanged("PerfomanceInfo");
+                RaisePropertyChanged("CanProcessing");
             }
-           
+            catch (Exception){}            
         }
 
+        private void SaveImage()
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.DefaultExt = ".png";
+            dlg.FileName = "Sigmented Image";
+            dlg.Title="Збереження сегментованого зображення";
+            dlg.Filter = "Image files (*.gif;*.jpg;*.jpe;*.png;*.bmp;*.dib;*.tif;*.tifF;*.wmf;*.ras;*.eps;*.pcx;*psd;*.tga)|*.gif;*.jpg;*.jpe;*.png;*.bmp;*.dib;*.tif;*.tifF;*.wmf;*.ras;*.eps;*.pcx;*psd;*.tga| JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|PNG Image|*.png";
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                SegmentedImage.Bitmap.Save(dlg.FileName);
+            }
+
+        }
 
         public void SegmentImage()
         {
@@ -191,7 +224,7 @@ namespace ImageSegmentation.ViewModel
                     {
                         GaussianFilter filter = new GaussianFilter();
                         filter.Filter(OriginImage.Bitmap.Width, OriginImage.Bitmap.Height, pixels, Sigma);
-                        int[,] segments = segmentation.BuildSegments(OriginImage.Bitmap.Width, OriginImage.Bitmap.Height, pixels, K, MinSize, Connection, DifType, ref perfomanceInfo);                       
+                        int[,] segments = segmentation.BuildSegments(OriginImage.Bitmap.Width, OriginImage.Bitmap.Height, pixels, K, MinSize, Connection, DifType, ref perfomanceInfo);
                         SegmentedImage = new ImageViewModel(ImageHelper.GetBitmap(segments));
                         RaisePropertyChanged("PerfomanceInfo");
                     }
@@ -204,7 +237,7 @@ namespace ImageSegmentation.ViewModel
             });
         }
 
-       
+
         protected void ClearPerfomanceInfo()
         {
             perfomanceInfo.AlgorithmPerfomance = 0;
@@ -229,8 +262,8 @@ namespace ImageSegmentation.ViewModel
                     if (pixels != null)
                     {
                         filter.Filter(OriginImage.Bitmap.Width, OriginImage.Bitmap.Height, pixels, Sigma);
-                        
-                            SegmentedImage = new ImageViewModel(ImageHelper.GetFilterBitmap(pixels));
+
+                        SegmentedImage = new ImageViewModel(ImageHelper.GetFilterBitmap(pixels));
                     }
                 }
                 catch { }
@@ -242,33 +275,38 @@ namespace ImageSegmentation.ViewModel
         }
 
 
-        private List<RelayCommand> _commands;
+        private List<RelayCommand> commands;
         private IList<RelayCommand> Commands
         {
             get
             {
-                if (_commands == null)
+                if (commands == null)
                 {
-                    _commands = new List<RelayCommand>();
-                    _commands.Add(OpenImageCommand);
-                    _commands.Add(SegmentImageCommand);
-                    _commands.Add(GaussianImageCommand);
+                    commands = new List<RelayCommand>();
+                    commands.Add(OpenImageCommand);
+                    commands.Add(SegmentImageCommand);
+                    commands.Add(GaussianImageCommand);
                 }
-                return _commands.AsReadOnly();
+                return commands.AsReadOnly();
             }
         }
 
+        public bool CanProcessing
+        {
+            get { return canNewExecute && OriginImage != null; }
 
-        private bool _canNewExecute = true;
+        }
+        private bool canNewExecute = true;
         public bool CanNewExecute
         {
-            get { return _canNewExecute; }
+            get { return canNewExecute; }
             set
             {
-                if (value != _canNewExecute)
+                if (value != canNewExecute)
                 {
-                    _canNewExecute = value;
+                    canNewExecute = value;
                     RaisePropertyChanged("CanNewExecute");
+                    RaisePropertyChanged("CanProcessing");
                 }
             }
         }
